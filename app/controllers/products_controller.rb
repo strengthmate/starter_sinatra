1bcbad6a3d23aa1081bb9d35fba7179caffbbf6b
errolgh <ewatsonrei@gmail.com>
2019-04-24 18:07:52 -0400

    last commit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,7 @@ require 'pry'
 class ProductsController < Sinatra::Base
 
     set :views, "app/views/products"
+    set :method_override, true
 
     get "/products" do
       @products = Product.all
@@ -13,7 +14,43 @@ class ProductsController < Sinatra::Base
       erb :new
     end
 
+    get "/products/:id" do
+        id = params[:id]
+        @product = Product.find(id)
+        erb :show
+    end
 
+    post "/products" do
+        category_id = params[:category]
+        # binding.pry
+        # category = Category.find(category_id)
+        category = Category.find_by(name: params[:category])
+        name = params[:name]
+        description = params[:description]
+        price = params[:price]
+        product = Product.create(category: category, name: name, description: description, price: price)
+        # product.save
+        #create is the same as doing new and save
+        redirect "/products/#{product.id}"
+    end
+
+    get "/products/:id/edit" do
+        @product = Product.find(params[:id])
+        erb :edit
+    end
+
+    patch "/products/:id" do
+        @product = Product.find(params[:id])
+        @product.update(category: category, name: name, description: description, price: price)
+        redirect "/products/#{@product.id}"
+    end
+
+    delete "/products/:id" do
+        binding.pry
+        @product = Product.find(params[:id])
+        @product.destroy
+        redirect "/products"
+    end
 
 
 
